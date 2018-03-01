bb1e6ff2a29643ff6d9038c696bcb17f4597522b
中野雄貴 <totoro.windows@outlook.com>
2018-03-01 14:16:07 +0900

    タイマーをセットできるように変更

---
+++ server.js
@@ -0,0 +1,18 @@
+// モジュールオブジェクトの初期化
+var fs = require("fs");
+var server = require("http").createServer(function(req, res) {
+     res.writeHead(200, {"Content-Type":"text/html"});
+     var output = fs.readFileSync("./chat.html", "utf-8");
+     res.end(output);
+}).listen(9500);  // portを指定
+
+var io = require("socket.io").listen(server);
+
+// イベントの定義
+io.sockets.on("connection", function (socket) {
+
+  // メッセージ送信カスタムイベント
+  socket.on("publish", function (data) {
+    io.sockets.emit("publish", {value:data.value});
+  });
+});
